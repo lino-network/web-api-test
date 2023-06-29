757ece1df1aec8781eddbd570660af28797e71b6
lianghongsr71 <lianghongsr71@126.com>
2023-06-29 18:56:35 +0800

    add testcase

--- loadData/payloadData.py
+++ loadData/payloadData.py
@@ -1,3 +1,8 @@
+def normal_header():
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    return headers
 
 
 def login(username, password):
@@ -5,9 +10,7 @@ def login(username, password):
         "operationName": "EmailLogin",
         "variables": {
             "email": username,
-            "password": password,
-            "recaptchaToken": "03AL8dmw8ysn8ewYpObge7klDuBHutVNnLIy9yfcbxPw9m8HpYuyQ_R7t_IWhBEP3fuV6ZGkq_Uv8a621oADJ6XlJxMjCojJWkY_rkDuXfr3jhZ53uPl7dQS3Q9k3sNvYeTYcrIGsLz8H-34SL__KJ1AAgRlhAYtM_4e1HzVnYTbNAaMvIK-6Qti38XsHNUXpGn9Fmkm3KLlcvOwI2kx95Wft8fFUCvz1ccopRI4NwxOLe_3Aa5MiokU2YcTX-bUjJ1XI_O0m6YtXhh9ieeI3yQCXpSph2NUb5h9g-dz_gGoHhVjBFa52Gp7vCZjqxkg7Jxw3GlTMKjivR-MjaHPhBxW1n3F8MQ84TpLJo4Y4UsXk-oEM04g7g47nNOi2kG3lU6FWTSYR8lzDTIz1OKeptr0jfQuuhAQ0QhQByVCORuTGQhrHpQ8LaGeaU30eaWkC9XvPBXriVjW22mMjZ1iYF7Zrj1d9S-sHXe5CCxlPVe5tFg4d5yTvUwvsc3x8rrbU9qjPXQtdzf60JLkEK9r-4OnzpwE-TXBvra_WeD8XIx5JaKPL7FLjIKkt5U9SHG_6kTbEdeeD0FJiG"
-            # "recaptchaToken": "03AL8dmw9MnDH_La_lUO2gjCrh5vSjqAWFJHHx_t84rj1v7lum-kGTKUmxIMqMB2OwQ6n1EXLFEwE9tvriY7x1Yxvig7RHHXXUSjHV32KATS2nBrB-Qvl_wzHGEDLoUvIW5S_PmgvlWMJA7ouc1m8FTwkRA-x0jbp0dENUP9mmL6ugCWZuw8dIH_Im9cc--GGQbpwz4z4p5pIWATd4LGmaqkIpfk2L-_6wusqPun0GN9zqprwOzn1aREuH4YhLCPx2ry-JVvlvgR7HgTMCxktPAbg2c59phmASqH72XZEPW8u8Slzfn8FxKI37zj0wk7sB4v_1JBcvykxtWEYrTyzZF4833Y6i75Ehct5S2NYAStCJAwhZOyvWkPqwZBLjpj_LYw6Ib6XP7dQn86AgxMUZPqdZ2lZ6SIIuM5kCthPcfo8hT4MdjNbEObzmcdMXrqmmVbOQ9YYZIRaQAHB6zrYJ49lYczLMb0Ngj3hE6Jpi7SY3EFssXhOR_R0r5ls-IWu0CaUyw0IDbc5lulLQKHMjSUDVKQtPic_Bbc-4TEGQdLTQxKolWvEZIbY"
+            "password": password
         },
         "extensions": {
             "persistedQuery": {
@@ -248,8 +251,6 @@ def give_away_claim(streamer_name):  # 用户抢宝箱
     return payload
 
 
-
-
 def me_clips_of_me():
     payload = {
         "operationName": "MeClipsOfMe",
@@ -303,14 +304,12 @@ def get_today_token_limit():  # 获取体现额度
 
 
 
-def home_page_list_recommendation():
+def homepage_carousels():
     payload = {
-        "operationName": "HomePageListRecommendation",
+        "operationName": "HomePageCarousels",
         "variables": {
             "first": 40,
             "after": "0",
-            "languageID": null,
-            "categoryID": null,
             "showNSFW": True,
             "userLanguageCode": "",
             "showMatureContent": True
@@ -318,24 +317,22 @@ def home_page_list_recommendation():
         "extensions": {
             "persistedQuery": {
                 "version": 1,
-                "sha256Hash": "35160b344e48b47fbc781b60f92f7e6bf1bfcdea1ba1a08b311be56fef38b7d2"
+                "sha256Hash": "1668c6da479e8bf5cbffdff4006228499d14ead02f29cdb53a7a31404e191067"
             }
         }
     }
     return payload
 
 
-def home_page_livestream():
+def homepage_livestream():
     payload = {
         "operationName": "HomePageLivestream",
         "variables": {
             "first": 20,
             "after": "0",
-            "languageID": null,
-            "categoryID": null,
             "showNSFW": True,
             "order": "TRENDING",
-            "userLanguageCode": "",
+            "userLanguageCode": "en",
             "showMatureContent": True
         },
         "extensions": {
@@ -348,7 +345,24 @@ def home_page_livestream():
     return payload
 
 
-def home_page_carousels():
+def homepage_categories():
+    payload = {
+        "operationName": "HomePageCategories",
+        "variables": {
+            "first": 15,
+            "languageID": 'null'
+        },
+        "extensions": {
+            "persistedQuery": {
+                "version": 1,
+                "sha256Hash": "386f2dbb71fa1d28e3c9bbff30e41beb7c09dd845b02dcf01c35856076e354dc"
+            }
+        }
+    }
+    return payload
+
+
+def homepage_list_recommendation():  # 推荐系统
     payload = {
         "operationName": "HomePageListRecommendation",
         "variables": {
@@ -368,16 +382,16 @@ def home_page_carousels():
     return payload
 
 
-def global_information_recommend():# 主页左边的推荐列表
+def homepage_global_information_recommend():  # 主页左边的推荐列表
     payload = {
-        "operationName": "HomePageLivestream",
+        "operationName": "GlobalInformationRecommend",
         "variables": {
-            "limit": 5
+            "limit": 30
         },
         "extensions": {
             "persistedQuery": {
                 "version": 1,
-                "sha256Hash": "3cffbf525fc41b7b8d6472e28b1b0a30ed94d3dc2d6a0f22bcb779d5361506bf"
+                "sha256Hash": "f58123b70a8319382f44c077489e4fca285c2b06bc318e46605eb79644a6b0f3"
             }
         }
     }
@@ -399,7 +413,6 @@ def homepage_nav_search_result(search_text):
         }
     }
     return payload
-
 
 def live_streams_languages():
     payload = {
@@ -497,3 +510,27 @@ def browser_register_notification(token):
         }
     }
     return payload
+
+
+class DaskboardAPI:
+    def MeDashboard(self, username):
+        payload = {
+              "operationName": "MeDashboard",
+              "variables": {
+                "isLoggedIn": True,
+                "input": {
+                  "operator": username,
+                  "operation": "",
+                  "first": 10,
+                  "after": ""
+                }
+              },
+              "extensions": {
+                "persistedQuery": {
+                  "version": 1,
+                  "sha256Hash": "fe703ef0933440b5ffdf75c5f7662bd99ce9687f8c7d9e29961c82b244c21704"
+                }
+              }
+            }
+        return payload
+
