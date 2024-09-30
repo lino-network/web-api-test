059e14ae0faefa4d1b40bf0a021863c2f027f9ed
lianghongsr71 <lianghongsr71@126.com>
2024-09-30 16:33:10 +0800

    add app case

--- tests/api/test_android_homepage.py
+++ tests/api/test_android_homepage.py
@@ -137,7 +137,7 @@ class TestAndroidHomePage:
 
     @allure.title('test_user_post')
     @allure.severity(allure.severity_level.NORMAL)
-    def test_user_post(self, get_config_data, get_viewer1_login_auth_header):
+    def test_android_user_post(self, get_config_data, get_viewer1_login_auth_header):
         """
         接口： UserPost
         检查用户信息接口
@@ -183,6 +183,177 @@ class TestAndroidHomePage:
                     assert 'totalReward' in livestream and livestream['totalReward'] is not None
                     assert 'language' in livestream and 'language' in livestream['language']
                     assert 'category' in livestream and 'title' in livestream['category']
+
+    @allure.title('test_android_user_panel')
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_android_user_panel(self, get_config_data, get_viewer1_login_auth_header):
+        """
+        接口： UserPanel
+        检查用户面板信息接口
+        """
+        streamer_name = "automation"  # 使用的主播名称
+        with allure.step('检查 UserPanel 接口'):
+            response_json = common.api_post(get_config_data['url'], get_viewer1_login_auth_header,
+                                                Payload.Android.android_UserPanel(streamer_name))
+            print(response_json)
+
+            with allure.step('检查是否没有错误'):
+                assert 'err' not in response_json
+
+            with allure.step('检查返回的 userByDisplayName 对象'):
+                user = response_json['data']['userByDisplayName']
+                assert user is not None
+
+                panels = user['panels']
+                assert panels is not None  # 检查面板列表的有效性
+
+                #with allure.step('检查 panels 不为空'):
+                    #assert len(panels) > 0
+                
+                with allure.step('检查每个 panel 的字段'):
+                    required_fields = ['title', 'imageURL', 'imageLinkURL', 'body']
+
+                    for panel in panels:
+                        for field in required_fields:
+                            assert field in panel and panel[field] is not None, f"{field} should not be None"
+
+
+    @allure.title('test_channel_emote')
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_channel_emote(self, get_config_data, get_viewer1_login_auth_header):
+        """
+        接口： ChannelEmote
+        检查频道表情接口
+        """
+        with allure.step('检查 ChannelEmote 接口'):
+            streamer_name = "automation"
+            response_json = common.api_post(get_config_data['url'], get_viewer1_login_auth_header,
+                                            Payload.Android.android_ChannelEmote(streamer_name))
+            print(response_json)
+
+            with allure.step('检查是否没有错误'):
+                assert 'err' not in response_json
+
+            with allure.step('检查返回的 user 对象'):
+                user = response_json['data']['user']
+                assert user is not None
+
+                emote = user['emote']
+                assert emote is not None
+
+                with allure.step('检查 emote 中的 vip 和 channel 是否都存在'):
+                    assert 'vip' in emote
+                    assert 'channel' in emote
+
+                    # 检查 vip 的列表
+                    vip_emotes = emote['vip']['list']
+                    assert vip_emotes is not None
+                    assert len(vip_emotes) > 0  # 确保不为空
+
+                    # 检查 channel 的列表
+                    channel_emotes = emote['channel']['list']
+                    assert channel_emotes is not None
+                    assert len(channel_emotes) > 0  # 确保不为空
+
+                    # 验证 vip emote 的字段
+                    for emote_item in vip_emotes:
+                        assert 'name' in emote_item and emote_item['name'] is not None
+                        assert 'level' in emote_item and emote_item['level'] is not None
+                        assert 'type' in emote_item and emote_item['type'] is not None
+                        assert 'username' in emote_item and emote_item['username'] == streamer_name
+                        assert 'sourceURL' in emote_item and emote_item['sourceURL'] is not None
+
+                    # 验证 channel emote 的字段
+                    for emote_item in channel_emotes:
+                        assert 'name' in emote_item and emote_item['name'] is not None
+                        assert 'level' in emote_item and emote_item['level'] is not None
+                        assert 'type' in emote_item and emote_item['type'] is not None
+                        assert 'username' in emote_item and emote_item['username'] == streamer_name
+                        assert 'sourceURL' in emote_item and emote_item['sourceURL'] is not None
+
+
+    @allure.title('test_android_SCMessages')
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_android_SCMessages(self, get_config_data, get_viewer1_login_auth_header):
+        """
+        接口： UserPost
+        检查用户信息接口
+        """
+        with allure.step('检查 SCMessages 接口'):
+            response_json = common.api_post(get_config_data['url'], get_viewer1_login_auth_header,
+                                            Payload.Android.android_SCMessages("automation"))
+            print(response_json)
+
+            with allure.step('检查是否没有错误'):
+                assert 'err' not in response_json
+
+            with allure.step('检查返回的 user 对象'):
+                user = response_json['data']['user']
+                assert user is not None
+
+                # 检查 chats 的有效性
+                chats = user['chats']
+                assert chats is not None
+
+                with allure.step('检查 chats 不为空'):
+                    assert len(chats) > 0
+
+                # 检查每条聊天信息的有效性
+                for chat in chats:
+                    #assert 'id' in chat
+                    assert 'type' in chat
+
+                    # 检查特定类型（ChatText)是否有必要字段
+                    if chat['__typename'] == 'ChatText':
+                        assert 'content' in chat and chat['content'] is not None
+                        assert 'sender' in chat and chat['sender'] is not None
+
+                        # 检查 sender 的字段
+                        sender = chat['sender']
+                        assert 'username' in sender and sender['username'] is not None
+                        assert 'displayname' in sender and sender['displayname'] is not None
+                        assert 'avatar' in sender and sender['avatar'] is not None
+
+                    # 其他聊天类型（如 ChatGift、ChatFollow 等），可以在这里检查是否有特定逻辑需要进行验证
+
+    @allure.title('test_android_total_contributor_query')
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_android_total_contributor_query(self, get_config_data, get_viewer1_login_auth_header):
+        """
+        接口： TotalContributorQuery
+        检查用户贡献者统计信息接口
+        """
+        with allure.step('检查 TotalContributorQuery 接口'):
+            response_json = common.api_post(get_config_data['url'], get_viewer1_login_auth_header,
+                                            Payload.Android.android_TotalContributorQuery("automation"))
+            print(response_json)
+
+            with allure.step('检查是否没有错误'):
+                assert 'err' not in response_json
+
+            with allure.step('检查返回的 user 对象'):
+                user = response_json['data']['user']
+                assert user is not None
+
+                top_contributions = user['topContributions']
+                assert top_contributions is not None
+
+                with allure.step('检查 topContributions 不为空'):
+                    contribution_list = top_contributions['list']
+                    assert contribution_list is not None and len(contribution_list) > 0
+
+                # 检查每个 contribution 的字段
+                with allure.step('检查每个贡献的字段'):
+                    for contribution in contribution_list:
+                        assert 'contributor' in contribution and contribution['contributor'] is not None
+                        assert 'amount' in contribution and contribution['amount'] is not None
+                        
+                        contributor = contribution['contributor']
+                        required_fields = ['username', 'displayname', 'avatar', 'partnerStatus', 'role']
+
+                        for field in required_fields:
+                            assert field in contributor and contributor[field] is not None, f"{field} should not be None"
+
 
 if __name__ == '__main__':
     print('e2rwf')
